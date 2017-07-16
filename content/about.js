c700868f1463ce62e7f19800576534d43719a79f
Mike <mgrahamjo@gmail.com>
2017-07-16 15:27:49 -0700

    fix broken email links on about page

--- content/about.js
+++ content/about.js
@@ -2,7 +2,7 @@ module.exports = `
 <h1>About PilotHound</h1>
 <p>PilotHound was created to facilitate the growth of the nacent UAV industry. We accomplish this by compiling and maintaining current comprehensive listings of educational and professional opportunities.</p>
 <h2>Submitting a Job Listing</h2>
-<p>All listings are currently free. To submit a new job listing, email <a href="hi@pilothound.com">hi@pilothound.com</a> with the following information:</p>
+<p>All listings are currently free. To submit a new job listing, email <a href="mailto:hi@pilothound.com">hi@pilothound.com</a> with the following information:</p>
 <ul>
     <li>Job Title</li>
     <li>Job summary</li>
@@ -11,7 +11,7 @@ module.exports = `
     <li>Job location (state)</li>
 </ul>
 <h2>Submit a course listing</h2>
-<p>To submit a new course listing, email <a href="hi@pilothound.com">hi@pilothound.com</a> with the following information (or as much of it as possible):</p>
+<p>To submit a new course listing, email <a href="mailto:hi@pilothound.com">hi@pilothound.com</a> with the following information (or as much of it as possible):</p>
 <ul>
     <li>School name</li>
     <li>School website</li>
@@ -28,5 +28,5 @@ module.exports = `
     <li>Course description</li>
 </ul>
 <h2>Report new or inaccurate information</h2>
-<p>Shoot us an email at <a href="hi@pilothound.com">hi@pilothound.com</a>. Thanks for the report!</p>
+<p>Shoot us an email at <a href="mailto:hi@pilothound.com">hi@pilothound.com</a>. Thanks for the report!</p>
 `;
